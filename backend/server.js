ec2f88cbc4b1793c1057c1f174796b747f7f3598
Arya Waskar <aryawaskar1@gmail.com>
2025-11-27 04:06:21 +0530

    all problems fixed

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
   "https://moodbloom-act-website.onrender.com",
+  "https://moodbloom-project-7w7pfr588-arya-waskars-projects.vercel.app",
 ];
 
 const corsOptions = {
