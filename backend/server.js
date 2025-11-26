cc15b4671190e30d6fdb5ac133cdfce689a35bc7
Arya Waskar <aryawaskar1@gmail.com>
2025-11-26 14:24:38 +0530

    Fix backend deploy for Render

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,15 @@ const authRoutes = require("./routes/authRoute");
 const actRoutes = require("./routes/act");
 const userProfile = require("./routes/profileRoute");
 const Act = require("./models/kindnessAct");
+const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+};
 
 
-
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
@@ -31,6 +36,6 @@ app.get("/", (req, res) => {
   res.send("working");
 });
 
-app.listen(5000, () => {
-  console.log("server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
